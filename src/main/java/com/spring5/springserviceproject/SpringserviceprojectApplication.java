125da6bbfd17ae84aa03b5634910964aa8b6404c
Sai Krishna <saikrishna05h1@gmail.com>
2020-05-03 16:15:52 +0530

    Initial Commit Add.

---
+++ src/main/java/com/spring5/springserviceproject/SpringserviceprojectApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.springserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringserviceprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringserviceprojectApplication.class, args);
+    }
+
+}
