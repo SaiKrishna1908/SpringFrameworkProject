cea10ebaa0cf6b664d58dcd2b73686ac02af7d0e
Sai Krishna <saikrishna05h1@gmail.com>
2020-05-30 19:49:49 +0530

    Added Visit Entity and getter and setter for Pet and Pet type, closes #29

--- spring-pet-data/src/main/java/com/spring5/springserviceproject/Model/PetType.java
+++ spring-pet-data/src/main/java/com/spring5/springserviceproject/Model/PetType.java
@@ -2,4 +2,12 @@ package com.spring5.springserviceproject.Model;
 
 public class PetType extends BaseEntity{
     String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
