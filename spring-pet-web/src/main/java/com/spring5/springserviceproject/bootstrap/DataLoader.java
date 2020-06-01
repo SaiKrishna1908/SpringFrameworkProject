6f50c0bdb07953d1408f5a129514e9fb30ff8ab2
Sai Krishna <saikrishna05h1@gmail.com>
2020-06-01 23:04:12 +0530

    Enhance Owners with contact info and Pets, closes #36

--- spring-pet-web/src/main/java/com/spring5/springserviceproject/bootstrap/DataLoader.java
+++ spring-pet-web/src/main/java/com/spring5/springserviceproject/bootstrap/DataLoader.java
@@ -1,15 +1,13 @@
 package com.spring5.springserviceproject.bootstrap;
 
-import com.spring5.springserviceproject.Model.Owner;
-import com.spring5.springserviceproject.Model.PetType;
-import com.spring5.springserviceproject.Model.Speciality;
-import com.spring5.springserviceproject.Model.Vet;
+import com.spring5.springserviceproject.Model.*;
 import com.spring5.springserviceproject.Service.OwnerService;
 import com.spring5.springserviceproject.Service.PetTypeService;
 import com.spring5.springserviceproject.Service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -32,14 +30,41 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Jackson");
         owner1.setSecondName("Florence");
+        owner1.setAddress("7th street ");
+        owner1.setCity("Hyderabad");
+        owner1.setTelephone("123897590");
 
+
+        PetType petType1 = new PetType();
+        petType1.setName("Dog");
+
+        Pet pet1 = new Pet();
+        pet1.setPetType(petType1);
+        pet1.setBirthday(LocalDate.now());
+        pet1.setOwner(owner1);
+        pet1.setName("Simba");
+
+        owner1.getPets().add(pet1);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Plimbo");
         owner2.setSecondName("dumbo");
+        owner2.setAddress("7th street ");
+        owner2.setCity("Hyderabad");
+        owner2.setTelephone("123897590");
 
+        PetType petType2 = new PetType();
+        petType2.setName("Cat");
+
+        Pet pet2 = new Pet();
+        pet2.setPetType(petType2);
+        pet2.setBirthday(LocalDate.now());
+        pet2.setOwner(owner2);
+        pet2.setName("Apollo");
+
+        owner2.getPets().add(pet2);
 
         ownerService.save(owner2);
 
