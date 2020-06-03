8b7f29e10b965b5ee96caebbe206dd06e245f904
Sai Krishna <saikrishna05h1@gmail.com>
2020-06-03 22:58:16 +0530

    Added SpecialityMapService, closes #34

---
+++ spring-pet-data/src/main/java/com/spring5/springserviceproject/MapService/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.spring5.springserviceproject.MapService;
+
+import com.spring5.springserviceproject.Model.Speciality;
+import com.spring5.springserviceproject.Service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends MapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long aLong) {
+         super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+         super.delete(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
