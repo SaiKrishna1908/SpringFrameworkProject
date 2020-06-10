eb58002cd1ab7f462b6d5ea7e4977c7e8fe30ebb
Sai Krishna <saikrishna05h1@gmail.com>
2020-06-10 15:50:58 +0530

    Vets JPA implemented, closes #40

--- spring-pet-data/src/main/java/com/spring5/springserviceproject/Model/Vet.java
+++ spring-pet-data/src/main/java/com/spring5/springserviceproject/Model/Vet.java
@@ -1,10 +1,15 @@
 package com.spring5.springserviceproject.Model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialties_id"))
     Set<Speciality> specialitySet = new HashSet<>();
 
     public Set<Speciality> getSpecialitySet() {
