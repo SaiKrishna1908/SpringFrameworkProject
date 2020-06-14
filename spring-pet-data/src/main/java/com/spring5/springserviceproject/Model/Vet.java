436a6a33e7a1a008fbc50e0f0f6fc68da41cb35a
Sai Krishna <saikrishna05h1@gmail.com>
2020-06-14 23:02:59 +0530

    Added Test's, closes #54

--- spring-pet-data/src/main/java/com/spring5/springserviceproject/Model/Vet.java
+++ spring-pet-data/src/main/java/com/spring5/springserviceproject/Model/Vet.java
@@ -1,22 +1,25 @@
 package com.spring5.springserviceproject.Model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "vet")
 public class Vet extends Person{
 
+    public Vet(String firstName, String lastName, Long id, Set<Speciality> specialities){
+        super(id, firstName,lastName);
+        this.specialitySet = specialities;
+    }
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialties_id"))
     Set<Speciality> specialitySet = new HashSet<>();
 
-    public Set<Speciality> getSpecialitySet() {
-        return specialitySet;
-    }
-
-    public void setSpecialitySet(Set<Speciality> specialitySet) {
-        this.specialitySet = specialitySet;
-    }
 }
